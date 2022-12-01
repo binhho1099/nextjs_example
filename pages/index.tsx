45e5b531f22eef78257ad47ac9ae1e70b093d44b
annminn100 <annminn100@gmail.com>
2022-12-01 14:30:56 +0700

    build: example storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from '@components/auth/login';
+import MonopolyCard from '@components/shared/cards/monopoly-card/MonopolyCard';
 import type { NextPage } from 'next';
 import React from 'react';
 // import { Button } from '../stories/Button';
@@ -7,6 +8,16 @@ const Home: NextPage = () => {
   return (
     <React.Fragment>
       <LoginForm />
+      <div style={{ maxWidth: '200px', margin: '0 auto' }}>
+        <MonopolyCard
+          imageSrc="/assets/test-main.jpg"
+          imageSeoTag="test"
+          title="test title"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veniam deleniti dolorum! Sint, provident sunt."
+          authorImage="/assets/test-main.jpg"
+          authorName="Author"
+        />
+      </div>
     </React.Fragment>
   );
 };
